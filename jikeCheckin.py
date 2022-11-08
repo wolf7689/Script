6a752b91676ac89e31502c9d72f837092620e8fa
wolf7689 <82700115+wolf7689@users.noreply.github.com>
2022-11-08 21:50:45 +0800

    Update jikeCheckin.py

--- jikeCheckin.py
+++ jikeCheckin.py
@@ -22,24 +22,25 @@ for c in ctext_a:
         cookies[kv[0].strip()] = kv[1].strip()
 
 url = 'https://jike191.com/user/checkin'
-headers = {
-    'user-agent': 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35'},
-    'referer': 'https://jike191.com/user'
-    }
+# headers = {
+#     'user-agent': 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+#     'referer': 'https://jike191.com/user'
+#     }
 
-r = requests.post(url, cookies=cookies, headers=headers)
+r = requests.post(url, cookies=cookies)
 title = "极客云签到"
 context = ""
 try:
     response = r.json()
     if(response.get('ret')):
-        context = "签到成功 " + 'msg'
+        context = "签到成功 " + response.get('msg')
         print(context)
     else:
-        context = "签到失败 " + 'msg'
+        context = "签到失败 " + response.get('msg')
         print(context)
 except:
+    context = r.text
     print("出错啦！！")
-    print(r.text)
+    print(context)
     
 send(title, context)
