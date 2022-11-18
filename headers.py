6c09ce978df79111ad8adbf3d4b63671d07eb8a8
wolf7689 <82700115+wolf7689@users.noreply.github.com>
2022-11-19 00:54:21 +0800

    Add files via upload

---
+++ headers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+headers1 = {
+    'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 '
+                  'Safari/537.36 Edg/107.0.1418.42 '
+}
+
+headers2 = {
+    'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 '
+                  'Safari/537.36 Edg/107.0.1418.42 '
+}
