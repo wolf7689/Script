a404150380490409507fb6e1e1412efda4969963
wolf7689 <82700115+wolf7689@users.noreply.github.com>
2022-11-20 02:08:42 +0800

    Update pt_checkin.py

--- pt_checkin.py
+++ pt_checkin.py
@@ -6,11 +6,15 @@
 export PT_TIME_COOKIE="网站cookie，分号;隔开" 
 export HDC_COOKIE="网站cookie" 
 export CHD_COOKIE="网站cookie" 
+export HDA_COOKIE="网站cookie" 
+export HD_TIME_COOKIE="网站cookie" 
+export TTG_COOKIE="网站cookie" 
 cron: 0 20 8 * * *
 new Env('PT签到');
 """
 
 import os
+import demjson3
 import requests
 import requests_html
 import headers
@@ -104,6 +108,82 @@ def chd():
     return return_msg
 
 
+def hd_area():
+    url = 'https://www.hdarea.co/sign_in.php'
+    session = requests_html.HTMLSession()
+    ctext = os.getenv("HDA_COOKIE")
+    cookies = generate_cookies(ctext)
+    payload = {'action': 'sign_in'}
+    r = session.post(url, cookies=cookies, headers=headers.headers1, data=payload)
+
+    if r.status_code == requests.codes.ok:
+        print(r.text)  # 纯文本
+        if len(r.html.find('title')) > 0:
+            return_msg = 'hdarea登录失效'
+            print(return_msg)
+        else:
+            return_msg = 'hdarea签到成功'
+            print(return_msg)
+    else:
+        return_msg = 'hdarea签到失败'
+        print(return_msg)
+    return return_msg
+
+
+def hd_time():
+    url = 'https://hdtime.org/attendance.php'
+    session = requests_html.HTMLSession()
+    ctext = os.getenv("HD_TIME_COOKIE")
+    cookies = generate_cookies(ctext)
+    r = session.get(url, cookies=cookies, headers=headers.headers1)
+
+    if r.status_code == requests.codes.ok:
+        # print(r.text)
+        if '登录' in r.html.find('title')[0].text:
+            return_msg = 'hdtime登录失效'
+            print(return_msg)
+        else:
+            return_msg = 'hdtime签到成功'
+            print(return_msg)
+    else:
+        return_msg = 'hdtime签到失败'
+        print(return_msg)
+    return return_msg
+
+
+def ttg():
+    main_url = 'https://totheglory.im/'
+    url = 'https://totheglory.im/signed.php'
+    session = requests_html.HTMLSession()
+    ctext = os.getenv("TTG_COOKIE")
+    cookies = generate_cookies(ctext)
+    r = session.get(main_url, cookies=cookies, headers=headers.headers1)
+    if r.status_code == requests.codes.ok:
+        r.encoding = 'utf-8'
+        print(r.text)
+        if '登录' in r.html.find('title')[0].text:
+            return_msg = 'ttg登录失效'
+            print(return_msg)
+        else:
+            json_text = r.text.split('$.post("signed.php", {')[1].split('},', 1)[0]
+            print(json_text)
+            payload = demjson3.decode('{' + json_text + '}')
+            r = session.post(url, cookies=cookies, headers=headers.headers1, data=payload)
+
+            if r.status_code == requests.codes.ok:
+                r.encoding = 'utf-8'
+                print(r.text)  # 纯文本
+                return_msg = 'ttg签到成功'
+                print(return_msg)
+            else:
+                return_msg = 'ttg签到失败'
+                print(return_msg)
+    else:
+        return_msg = 'ttg签到失败'
+        print(return_msg)
+    return return_msg
+
+
 if __name__ == '__main__':
     ln = '\n'
     title = 'pt签到'
@@ -111,4 +191,7 @@ if __name__ == '__main__':
     context = context + pt_time() + ln
     context = context + hd_china() + ln
     context = context + chd() + ln
+    context = context + hd_area() + ln
+    context = context + hd_time() + ln
+    context = context + ttg() + ln
     send(title, context)
