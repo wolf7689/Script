38e0963829361ded044a8442d68f0fad58d74a58
wolf7689 <82700115+wolf7689@users.noreply.github.com>
2022-11-19 22:56:03 +0800

    Update pt_checkin.py

--- pt_checkin.py
+++ pt_checkin.py
@@ -5,6 +5,7 @@
 环境变量说明：
 export PT_TIME_COOKIE="网站cookie，分号;隔开" 
 export HDC_COOKIE="网站cookie" 
+export CHD_COOKIE="网站cookie" 
 cron: 0 20 8 * * *
 new Env('PT签到');
 """
@@ -12,7 +13,6 @@ new Env('PT签到');
 import os
 import requests
 import requests_html
-from requests import JSONDecodeError
 import headers
 from notify import send
 
@@ -49,27 +49,58 @@ def hd_china():
     cookies = generate_cookies(ctext)
     r = session.get(main_url, cookies=cookies, headers=headers.headers2)
     if r.status_code == requests.codes.ok:
-        csrf = r.html.find('meta[name="x-csrf"]')[0].attrs['content']
-        # print(csrf)
-        payload = {'csrf': csrf}
-        r = session.post(url, cookies=cookies, headers=headers.headers2, data=payload)
+        if 'Login' in r.html.find('title')[0].text:
+            return_msg = 'hdchina登录失效'
+            print(return_msg)
+        else:
+            csrf = r.html.find('meta[name="x-csrf"]')[0].attrs['content']
+            # print(csrf)
+            payload = {'csrf': csrf}
+            r = session.post(url, cookies=cookies, headers=headers.headers2, data=payload)
 
-        if r.status_code == requests.codes.ok:
-            # print(r.text)
-            try:
+            if r.status_code == requests.codes.ok:
                 print(r.json())
                 return_msg = 'hdchina签到成功'
                 print(return_msg)
-            except JSONDecodeError:
-                return_msg = 'hdchina登录失效'
+            else:
+                return_msg = 'hdchina签到失败'
                 print(return_msg)
-        else:
-            return_msg = 'hdchina签到失败'
-            print(return_msg)
     else:
         return_msg = 'hdchina签到失败'
         print(return_msg)
+    return return_msg
 
+
+def chd():
+    url = 'https://chdbits.co/bakatest.php'
+    session = requests_html.HTMLSession()
+    ctext = os.getenv("CHD_COOKIE")
+    cookies = generate_cookies(ctext)
+    r = session.get(url, cookies=cookies, headers=headers.headers1)
+    if r.status_code == requests.codes.ok:
+        # print(r.text)
+        if '登录' in r.html.find('title')[0].text:
+            return_msg = 'chd登录失效'
+            print(return_msg)
+        else:
+            question_id = r.html.find('input[name="questionid"]')[0].attrs['value']
+            payload = {
+                'questionid': question_id,
+                'choice[]': 1,
+                'usercomment': '此刻心情:无',
+                'wantskip': '不会'
+            }
+            r = session.post(url, cookies=cookies, headers=headers.headers1, data=payload)
+            if r.status_code == requests.codes.ok:
+                # print(r.text)
+                return_msg = 'chd签到成功'
+                print(return_msg)
+            else:
+                return_msg = 'chd签到失败'
+                print(return_msg)
+    else:
+        return_msg = 'chd签到失败'
+        print(return_msg)
     return return_msg
 
 
@@ -79,4 +110,5 @@ if __name__ == '__main__':
     context = ln
     context = context + pt_time() + ln
     context = context + hd_china() + ln
+    context = context + chd() + ln
     send(title, context)
